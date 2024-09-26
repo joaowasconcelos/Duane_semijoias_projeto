c2fc345ea54a8f1b5d57576cd82eb9ab3823a8ed
Kevin Moreira <kfmsilva@gmail.com>
2024-09-25 21:44:10 -0300

    estrutura de telas e pagina login

--- mobile/appDuane/App.js
+++ mobile/appDuane/App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import CadastroFuncionario from './pages/cadastroFuncionario';
+import ConsultaEdicaoFuncionario from './pages/consultaEdicaoFuncionario';
+import CadastroProdutos from './pages/cadastroProdutos';
+import CatalagoProdutos from './pages/catalagoProdutos';
+import Cupons from './pages/cupons';
+import CadastroNovoCupom from './pages/cadastroNovoCupom';
+import DetalhesCupom from './pages/detalhesCupom';
+import DetalhesPromocao from './pages/detalhesPromocao';
+import GerarPromocao from './pages/gerarPromocao';
+import Home from './pages/home';
+import Login from './pages/login';
+import Pedidos from './pages/pedidos';
+import Promocoes from './pages/promocoes';
+import RelatorioVendas from './pages/relatorioVendas';
+import Clientes from './pages/clientes';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
