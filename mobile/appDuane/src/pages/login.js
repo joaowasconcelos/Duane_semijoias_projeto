14a35c1a8bc7389b50d90dc9209dd673a87e481d
Kevin Moreira <kfmsilva@gmail.com>
2024-09-27 20:41:24 -0300

    progredindo página login

--- mobile/appDuane/src/pages/login.js
+++ mobile/appDuane/src/pages/login.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet, TouchableOpacity, Image, Platform, ScrollView } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image, Platform, ScrollView, TextInput } from "react-native";
 import {useNavigation } from "@react-navigation/native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -12,11 +12,6 @@ import {
   EBGaramond_600SemiBold,
   EBGaramond_700Bold,
   EBGaramond_800ExtraBold,
-  EBGaramond_400Regular_Italic,
-  EBGaramond_500Medium_Italic,
-  EBGaramond_600SemiBold_Italic,
-  EBGaramond_700Bold_Italic,
-  EBGaramond_800ExtraBold_Italic,
 } from '@expo-google-fonts/eb-garamond';
 
 // import {getStatusBarHeight} from "react-native-status-bar-height";
@@ -32,11 +27,6 @@ export default function Login(){
         EBGaramond_600SemiBold,
         EBGaramond_700Bold,
         EBGaramond_800ExtraBold,
-        EBGaramond_400Regular_Italic,
-        EBGaramond_500Medium_Italic,
-        EBGaramond_600SemiBold_Italic,
-        EBGaramond_700Bold_Italic,
-        EBGaramond_800ExtraBold_Italic,
     });
 
     if (!fontsLoaded) {
@@ -45,36 +35,33 @@ export default function Login(){
 
         return(
             <SafeAreaView style={styles.androidSafeArea}>
-                <ScrollView>
-                    <View style={styles.container}>
-                        <Image source={require("../assets/ondas-rosa-header.png")} style={{width: '100%', height: 200, position: 'absolute',top:0}}/>
-                        <View>
-                            <View>
-                                <Image source={require("../assets/Duane.png")} style={{width: 120, height: 120}}/>
-                                <Text >Seja Bem-vinda!</Text>
-                            </View>
-
-                            <View>
-                                <View>
-                                    <Text>Email:</Text>
-                                    <TextInput></TextInput>
-                                </View>
-                                <View>
-                                    <Text>Senha:</Text>
-                                    <TextInput secureTextEntry={true}></TextInput>
-                                </View>
-
-                                
-                            </View>
-                            
-                            
-                            
-                            
-                            
+                <View style={styles.container}>
+                    <Image source={require("../../assets/ondas-rosa-header.png")} style={{width: '100%', height: 200, position: 'absolute',top:0}}/>
+                    <View style={{flex:1,}}>
+                        <View style={{alignItems: "center", margin: 50}}>
+                            <Image source={require("../../assets/Duane.png")} style={{width: 120, height: 120}}/>
+                            <Text style={{fontFamily: 'EBGaramond_800ExtraBold', fontSize: 32, color: '#C4914D', opacity: 0.7}}>Seja Bem-vinda!</Text>
                         </View>
-                        <Image source={require("../assets/ondas-rosa-footer.png")} style={{width: '100%', height: 210, position: 'absolute', bottom: 0}}/>
+
+                        <View>
+                            <Text style={{fontFamily: 'EBGaramond_800ExtraBold', fontSize: 22, color: '#765555'}}>Realize o login com as suas credencias</Text>
+                            <View>
+                                <Text>Email:</Text>
+                                <TextInput></TextInput>
+                            </View>
+                            <View>
+                                <Text>Senha:</Text>
+                                <TextInput secureTextEntry={true}></TextInput>
+                                <Text style={{textAlign: 'center'}} onPress={()=>{}}>Esqueceu sua senha?</Text>
+                            </View>
+                            
+                                
+                        </View>
+                            
                     </View>
-                </ScrollView>
+                    <Image source={require("../../assets/ondas-rosa-footer.png")} style={{width: '100%', height: 210, position: 'absolute', bottom: 0}}/>
+                </View>
+                
             </SafeAreaView>
         )
 
