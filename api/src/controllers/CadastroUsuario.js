14a94642f82796edc0ce01f3b61b83cb5dfcb105
unknown <murilo.aguiar2303@gmail.com>
2024-09-27 21:43:25 -0300

    Terminando cadastro do usuario

---
+++ api/src/controllers/CadastroUsuario.js
@@ -0,0 +1,70 @@
+import Login from "../model/Login.js";
+import Pessoa from "../model/Pessoa.js"
+import Telefone from "../model/Telefone.js";
+
+/**
+ * CadastroUsuario / CadastroPessoa ....
+ */
+const CadastroUsuario = {
+    //Cadastrar a pessoa (Perfil)
+    CadastroPessoa: async (req, res) => {
+        try {
+            const { Nome, Data_Nasc, CPF, Genero, Usuario, Senha, Telefones } = req.body;
+            const cPessoa = new Pessoa(null, Nome, Data_Nasc, CPF, Genero);
+            //Chamar o crud 
+            const insertPessoa = await cPessoa.CadastrarPessoa();
+            let insertTele;
+            if (!insertPessoa.error) {
+                const cLogin = new Login(null, Usuario, Senha, 0, 1, 2, insertPessoa);
+                console.log(cLogin)
+                const insertLogin = await cLogin.CadastrarLogin();
+
+                if (!insertLogin.error) {
+                    if (Telefones.length > 0) {
+                        for (const numeroTelefone of Telefones) {
+                            const novoTelefone = new Telefone(null, numeroTelefone, insertPessoa);
+                            insertTele = await novoTelefone.CadastrarTelefone();
+                            if (insertTele.error) {
+                                const deleteLogin = cLogin.DeletarLogin();
+                                const deletarPessoa = cPessoa.DeletarPessoa();
+                                res.status(400).json({ message: "Erro ao cadastrar Numero!" });
+                                return
+                            }
+                        };
+                    }
+                } else {
+                    const deletarPessoa = cPessoa.DeletarPessoa()
+                    res.status(400).json({ message: "Erro ao cadastrar Login!" });
+                }
+                res.status(201).json({ message: "Usuário cadastrado com sucesso!" });
+            } else {
+                res.status(400).json({ message: "Erro ao cadastrar usuário!" });
+            }
+
+
+        } catch (error) {
+            console.error(error); // Para debugar o erro
+            res.status(500).json({ error: "Erro ao cadastrar o usuário" });
+        }
+    },
+    EditarPessoa: async (req, res) => {
+        try {
+            const id = req.params;
+            const { Nome, Data_Nasc, CPF, Usuario, Telefones } = req.body;
+            const cPessoa = new Pessoa(null,Nome,Data_Nasc,CPF);
+            const cLogin = new Login(null, Usuario,)
+            if (Telefones.length > 0) {
+                for (const numeroTelefone of Telefones) {
+                    const novoTelefone = new Telefone(null, numeroTelefone, id);
+                    insertTele = await novoTelefone.CadastrarTelefone();
+                    
+                };
+            }
+        } catch (error) {
+            
+        }
+    }
+}
+
+
+export default CadastroUsuario
--- api/src/model/Telefone.js
+++ api/src/model/Telefone.js
@@ -44,10 +44,12 @@ console.log(this.Numero)
                 const telefoneResult = await bd.query('INSERT INTO telefone (numero) VALUES (?)', [this.Numero]);
                 const tel = (telefoneResult[0].insertId);
                 console.log('ID do Telefone:', tel);
+                
 
                 const telefoneHasPessoaResult = await bd.query("INSERT INTO telefone_has_pessoa (telefone_id,pessoa_id) VALUES (?,?)",
                     [tel,this.ID_pessoa]);
                 console.log("Inseriu Pessoa e Telefone")
+                return tel
         }
         catch (error) {
             console.log('Erro na transação:', error);
