7018237064b6da0ce6bd95357b46bdbb73e4207d
Thais <thaisachmidtdasilva@gmail.com>
2024-10-08 21:43:20 -0300

    alterando cadastro de produtos

--- web/publico/js/admInicio.js
+++ web/publico/js/admInicio.js
@@ -1,17 +1,3 @@
-// Carregar cabeçalho HTML
-fetch('./html/partials/headerAdm.html')
-    .then(response => response.text())
-    .then(html => {
-        const header = document.createElement('header');
-        header.innerHTML = html;
-        document.body.appendChild(header);
-    })
-    .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
-
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
-
 function changeSubtitle() {
     document.getElementById('subtitleCadastradas').textContent = 'Qual o novo nome da categoria?';
     document.getElementById('titleCadastradas2').textContent = 'Edite a categoria escolhida';
--- web/publico/js/cadastrarNovoProduto.js
+++ web/publico/js/cadastrarNovoProduto.js
@@ -1,13 +1,48 @@
-fetch('./html/partials/headerAdm.html')
-    .then(response => response.text())
-    .then(html => {
-        const header = document.createElement('header');
-        header.innerHTML = html;
-        document.body.appendChild(header);
-    })
-    .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
+//dinheiro
 
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
+// const mascaraMoeda = (event) => {
+//     const onlyDigits = event.target.value
+//         .split("")
+//         .filter(s => /\d/.test(s))
+//         .join("")
+//         .padStart(3, "0")
+//     const digitsFloat = onlyDigits.slice(0, -2) + "." + onlyDigits.slice(-2)
+//     event.target.value = maskCurrency(digitsFloat)
+// }
 
+// const maskCurrency = (valor, locale = 'pt-BR', currency = 'BRL') => {
+//     return new Intl.NumberFormat(locale, {
+//         style: 'currency',
+//         currency
+//     }).format(valor)
+// }
+
+const mascaraMoeda = (event) => {
+    const valor = event.target.value.replace(",", ".");
+    const onlyDigits = valor
+        .split("")
+        .filter(s => /\d/.test(s))
+        .join("")
+        .padStart(3, "0")
+    const digitsFloat = onlyDigits.slice(0, -2) + "." + onlyDigits.slice(-2)
+    event.target.value = maskCurrency(digitsFloat)
+}
+
+const maskCurrency = (valor, locale = 'pt-BR', currency = 'BRL') => {
+    return new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency
+    }).format(valor)
+}
+
+// utilizar somente numeros
+
+function somenteNumeros(e) {
+    var tecla = e.which || e.keyCode;
+
+    if ((tecla >= 48 && tecla <= 57) || tecla == 8) {
+        return true;
+    } else {
+        return false;
+    }
+}
