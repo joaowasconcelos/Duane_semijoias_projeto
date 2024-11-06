ec6b097f10bbad4bf7712913c1ecd356bf70018f
Your Name <you@example.com>
2024-11-06 20:48:56 -0300

    meus enderecos estatico

--- web/publico/js/checkUserProfile.js
+++ web/publico/js/checkUserProfile.js
@@ -2,14 +2,14 @@ function checkUserProfile() {
     const userProfile = localStorage.getItem('userProfile');
     if (!userProfile) {
         showNotification('Você não está autenticado! Redirecionando para a página de login.');
-        window.location.href = 'login.html';
+        window.location.href = '/web/publico/html/login.html';
         return;
     }
 
     const profileData = JSON.parse(userProfile);
     if (profileData.perfil != 1) {
         showNotification('Você não tem permissão para acessar esta página.');
-        window.location.href = '/publico/html/login.html'; 
+        window.location.href = '/web/publico/html/login.html'; 
     }
 
 }
