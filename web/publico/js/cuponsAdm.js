38806df805fc6017bc471361e92b88fd1a1a9d89
Thais <thaisachmidtdasilva@gmail.com>
2024-11-14 21:46:32 -0300

    cupons (pronto), falta modal de detalhes do produto e função de deletar produto

--- web/publico/js/cuponsAdm.js
+++ web/publico/js/cuponsAdm.js
@@ -33,6 +33,7 @@ function criarTabela() {
             <td class="tituloTd">Código</td>
             <td class="tituloTd">Status</td>
             <td></td>
+            <td></td>
         </tr>
     </thead>`;
 }
@@ -43,8 +44,14 @@ function carregaCupons(responseCupom) {
             `<tr>
             <td data-id="${this['id']}" class="dados">${this['codigo']}</td>
             <td class="dados">${this['status']}</td>
-            <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter" onclick="dadosModal('${this['id']}')">
+            <td>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter" onclick="dadosModal('${this['id']}')">
                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                </svg>
+            </td>
+            <td>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16" onclick="deletaCupom('${this['id']}')">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
                 </svg>
             </td>
         </tr>`;
@@ -98,12 +105,12 @@ function limparInput() {
 //cadastrar cupom
 
 async function salvar(event) {
-
     event.preventDefault();
-
     const token = localStorage.getItem('token');
-
-    console.log(token);
+    const Codigo = document.getElementById("Codigo").value;
+    const Descricao = document.getElementById("Descricao").value;
+    const Valor = document.getElementById("Valor").value;
+    const Quantidade = document.getElementById("Quantidade").value;
 
     console.log(Codigo, Descricao, Valor, Quantidade)
 
@@ -125,8 +132,10 @@ async function salvar(event) {
             console.log(response)
             limparInput()
             showNotification(response.data.message)
-            window.location.reload(true);
-        }).catch(error => {
+            setTimeout(() => {
+                window.location.reload(true);
+            }, 2000)          
+        }).catch(error =>{
             console.log(error)
         })
 
@@ -183,8 +192,6 @@ async function saveModal() {
     const Valor = document.getElementById("valor").value;
     const Quantidade = document.getElementById("quantidade").value;
 
-
-
     try {
         await axios.put(`${localStorage.getItem("ip")}ModificaCupom/${id}`,
             {
@@ -211,4 +218,33 @@ async function saveModal() {
         showNotification("Ocorreu um erro ao editar cupom. Tente novamente.");
     }
 
+}
+
+//excluir cupom 
+
+async function deletaCupom(id) {
+
+    console.log("chegou")
+    const token = localStorage.getItem('token');
+
+    try {
+        await axios.put(`${localStorage.getItem("ip")}DeletaCupom/${id}`,
+            {},
+            {
+                headers: {
+                    'x-access-token': token
+                }
+            }
+        ).then(response => {
+            showNotification(response.data.message)
+            setTimeout(() => {
+                window.location.reload(true);
+            }, 2000)
+        }).catch(error => {
+            showNotification(error.response.data.error);
+        });
+    } catch (error) {
+        console.error('Erro ao excluir cupom:', error);
+        showNotification("Ocorreu um erro ao excluir cupom. Tente novamente.");
+    }
 }
